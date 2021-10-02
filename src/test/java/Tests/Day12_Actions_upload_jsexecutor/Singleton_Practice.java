3e7a5fe50db29d039726e235ee2d87dddd71dacc
Zukhra Khaknazarova <zukhra2011@gmail.com>
2021-10-02 15:24:22 -0400

    Singleton Practice

--- src/test/java/Tests/Day12_Actions_upload_jsexecutor/Singleton_Practice.java
+++ src/test/java/Tests/Day12_Actions_upload_jsexecutor/Singleton_Practice.java
@@ -10,9 +10,15 @@ public class Singleton_Practice {
 
         String str2=Singleton.getWord();
         System.out.println("str2 = " + str2);
-
+//
         String str3=Singleton.getWord();
         System.out.println("str3 = " + str3);
 
+    }
+    
+    @Test
+    public void singleton_test2(){
+        String  str4=Singleton.getWord();
+        System.out.println("str4 = " + str4);
     }
 }
