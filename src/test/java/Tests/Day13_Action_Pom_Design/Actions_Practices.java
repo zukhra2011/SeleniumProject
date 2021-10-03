f0fad4baba886d2a1ff60e92c539092ccc2490b3
Zukhra Khaknazarova <zukhra2011@gmail.com>
2021-10-03 18:09:01 -0400

    Double Clicking (Iframe)

---
+++ src/test/java/Tests/Day13_Action_Pom_Design/Actions_Practices.java
@@ -0,0 +1,10 @@
+package Tests.Day13_Action_Pom_Design;
+
+import org.testng.annotations.Test;
+
+public interface Actions_Practices {
+    @Test
+    public void p2_double_click_test(){
+
+    }
+}
