bd901193d5d6607e3e7a052c5559af2628d8c655
Zukhra Khaknazarova <zukhra2011@gmail.com>
2021-10-04 19:19:20 -0400

    Waits practice.

---
+++ src/test/java/Tests/Day14_Pom_Synchronization/WebeDriver_Wait_practice.java
@@ -0,0 +1,15 @@
+package Tests.Day14_Pom_Synchronization;
+
+import org.testng.annotations.Test;
+
+public class WebeDriver_Wait_practice {
+    @Test
+    public void dynamic_loading_test7(){
+        //Go to http://practice.cybertekschool.com/dynamic_loading/7
+        //Wait until title is "Dynamic title"
+        //Assert:Message "Done" is displayed.
+        //Assert:Image is displayed.
+
+
+
+}}
